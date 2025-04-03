d2248711edc844f078d4bfd1968d12cfae7d24b0
sandersa <aric.sanders@nist.gov>
2025-04-02 21:58:40 -0600

    increment version

--- experimental_design/__init__.py
+++ experimental_design/__init__.py
@@ -21,7 +21,7 @@ import sys
 # Module Constants
 VERBOSE_IMPORT = True
 TIMED_IMPORT = True
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 "Constant that determines if import statements are echoed to output"
 # The new module load scheme can be for module in DE_API_MODULES.keys()
 # -----------------------------------------------------------------------------
